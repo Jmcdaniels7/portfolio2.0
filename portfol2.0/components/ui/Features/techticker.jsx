895e94a30cc223046752ed3682397b8b366bbf96
Jamcda <jamcdaniels99@gmail.com>
2025-09-01 15:53:03 -0400

    added more spacing between images in techticker

--- portfol2.0/components/ui/Features/techticker.jsx
+++ portfol2.0/components/ui/Features/techticker.jsx
@@ -6,7 +6,7 @@ export const TechTicker = ({ logos = [] }) => {
 
     return (
     <div className="overflow-hidden w-full py-6 bg-gray-900">
-      <div className="ticker-animation space-x-6">
+      <div className="ticker-animation space-x-10">
         {logos.map((logo, index) => (
           <div key={index} className="flex-shrink-0">
             <Image
